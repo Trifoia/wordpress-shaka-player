1a7520a9ef0579625d991e5a7c3aa9e1493ea7c4
Joshua Evans <thejoshuaevans@gmail.com>
2020-06-04 19:38:21 -0700

    Typo Fix v2

--- shaka-player/php/embed-html-factory.php
+++ shaka-player/php/embed-html-factory.php
@@ -1,12 +1,7 @@
 <?php
 
 function embed_html_factory($atts) {
-  $embedHtml = <<<EOD
-<video id="video"
-  width="$atts['width']"
-  poster="$atts['poster']"
-  controls autoplay></video>
-EOD;
+  $embedHtml = '<video id="video" width="' . $atts['width'] . '" poster="' . $atts['poster'] . '" controls autoplay></video>';
 
   print_r($embedHtml);
   return $embedHtml;

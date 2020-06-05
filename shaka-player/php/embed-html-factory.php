9635fc003f6a2dc4030f7a1a5b7ed8366786c139
Joshua Evans <thejoshuaevans@gmail.com>
2020-06-04 21:53:40 -0700

    Good Ol' Simi-colon gotcha fix

--- shaka-player/php/embed-html-factory.php
+++ shaka-player/php/embed-html-factory.php
@@ -1,7 +1,7 @@
 <?php
 
 function embed_html_factory($atts) {
-  $id = 'id=video ' // ID is always video
+  $id = 'id=video '; // ID is always video
   $source = 'manifestUri=' . $atts['source'] . ' '; // A source must always be present
 
   $width = $atts['width'] ? 'width=' . $atts['width'] . ' ' : '';

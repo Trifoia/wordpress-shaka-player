f5c0ff580a6b3da96f02fd9ab8f3d83cd2812b3d
Joshua Evans <thejoshuaevans@gmail.com>
2020-06-04 22:28:21 -0700

    Include Settings - Oops

--- shaka-player/shaka-player.php
+++ shaka-player/shaka-player.php
@@ -3,18 +3,19 @@
   Plugin Name: Shaka Player
   Plugin URI: https://github.com/Trifoia/wordpress-shaka-player
   description: Adds shortcodes that only display content when the user is logged in / out
-  Version: 0.1.4
+  Version: 0.1.5
   Author: Trifoia
   Author URI: https://trifoia.com
 */
 
 define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-include( PLUGIN_PATH . 'php/embed-html-factory.php');
+include( PLUGIN_PATH . 'php/embed-html-factory.php' );
+include( PLUGIN_PATH . 'settings.php' );
 
 /**
  * Shortcode used for embedding the Shaka Player
  */
-function shaka_player_shortcode($atts) {
+function shaka_player_shortcode( $atts ) {
   $a = shortcode_atts( array(
     'source' => NULL,
     'width' => NULL,
@@ -35,6 +36,6 @@ function shaka_player_shortcode($atts) {
   wp_enqueue_script( 'init-shaka-player-js', plugin_dir_url(__FILE__) . 'js/init-shaka-player.js' );
 
   // Return the generated embed code
-  return embed_html_factory($a);
+  return embed_html_factory( $a );
 }
-add_shortcode('shaka_player', 'shaka_player_shortcode');
+add_shortcode( 'shaka_player', 'shaka_player_shortcode' );
